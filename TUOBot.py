bfd4d1ad9c66150a919e86bd1fe98a81d4d6756f
jacobcreech <mrjacobcreech@gmail.com>
2015-08-05 23:41:55 -0500

    Improved imagegrab and coords
    Implemented coords to click on for the bot, as well as a random
    selection to bypass a bot detection system. The coords are also adjust
    based on the current screen, and will find the game on the screen based
    on the specific rgb of a pixel.

--- TUOBot.py
+++ TUOBot.py
@@ -6,9 +6,39 @@ import win32api
 import win32con
 from PIL import ImageOps
 from PIL import ImageGrab
+from random import randint
+import coord as Coord
+from coord import *
 
+x_pad = 1085
+y_pad = 527
+
+per_adjust = 1.46
+
+def left_click():
+	win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
+	time.sleep(.1)
+	win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
+	print "click"
+	
+def mouse_pos(coord):
+	win32api.SetCursorPos((x_pad + coord[0], y_pad + coord[1]))
+	
+def get_coords():
+	x,y = win32api.GetCursorPos()
+	x = x * per_adjust - x_pad
+	y = y * per_adjust - y_pad
+	print x,y
+	
+def random_coords(coord_set):
+	x_start, y_start, x_end, y_end = coord_set
+	x_pos = randint(x_start, x_end)
+	y_pos = randint(y_start, y_end)
+	return x_pos, y_pos
+	
+	
 def get_play_area():
-	TOP_LEFT_PIXELS = (192,190,167)
+	TOP_LEFT_PIXELS = (62,50,45)
 	GREY_BORDER = (0,0,0)
 	SCREEN_WIDTH = 1200
 	SCREEN_HEIGH = 900
@@ -22,16 +52,18 @@ def get_play_area():
 			# getdata returns a flat array, so the below figures out
 			# the 2d coords based on the index position.
 			top = (index / imageWidth)
-			left = (index % imageWidth)
-			if (im.getpixel((left + 1, top + 1)) == GREY_BORDER and
-				im.getpixel((left + 2, top + 2)) == GREY_BORDER):
-				top += 5
-				left += 5
-				
-				return (left, top, left + SCREEN_WIDTH, top + SCREEN_HEIGH) 
+			left = (index % imageWidth)	
+			x_pad = left - 1
+			y_pad = top - 1
+			return (x_pad, y_pad - 1, left + SCREEN_WIDTH, top + SCREEN_HEIGH) 
 
 	raise Exception("Play area not in view. " 
 			"Make sure the game is visible on screen!")
+		
+		
+def choose_player():
+	mouse_pos(random_coords(Coord.pvp[randint(0,5)]))
+	left_click()
 		
 def find_state():
 	'''
@@ -45,12 +77,17 @@ def find_state():
 	'''
 	pass
 		
+		
 def run():
 	find_state()
 	pass
 		
 def main():
-	get_play_area()
+	#get_play_area()
+	#get_coords()
+	#mouse_pos(random_coords(Coord.menu_mission))
+	#box = get_play_area()
+	#im = ImageGrab.grab(box)
 	while True:
 		run()
 	
--- quickGrab.pyw
+++ quickGrab.pyw
@@ -2,9 +2,14 @@ from PIL import ImageGrab
 import os
 import time
  
+# 1085, 527
+x_pad = 1084
+y_pad = 526
+# 2286, 1428 
+ 
 def screenGrab():
-    box = ()
-    im = ImageGrab.grab()
+    box = (x_pad + 1, y_pad + 1, x_pad + 1202, y_pad + 902)
+    im = ImageGrab.grab(box)
     im.save(os.getcwd() + '\\full_snap__' + str(int(time.time())) +
 '.png', 'PNG')
  
