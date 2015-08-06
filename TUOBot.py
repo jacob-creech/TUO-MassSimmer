6108030ab40bea42b852f72c7a221d03b19e6e71
jacobcreech <mrjacobcreech@gmail.com>
2015-08-06 07:00:32 -0500

    Added pvp implementation
    Bot now does pvp automatically. Will stop running for 1-2 hours after
    each run, randomized in order to look unlike a bot.

--- TUOBot.py
+++ TUOBot.py
@@ -4,14 +4,21 @@ import sys
 import time
 import win32api
 import win32con
+import array
 from PIL import ImageOps
 from PIL import ImageGrab
 from random import randint
+from random import uniform
 import coord as Coord
 from coord import *
+from numpy import *
 
 x_pad = 1085
 y_pad = 527
+win = 0
+lose = 0
+quit = False
+
 
 per_adjust = 1.46
 
@@ -19,7 +26,6 @@ def left_click():
 	win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
 	time.sleep(.1)
 	win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
-	print "click"
 	
 def mouse_pos(coord):
 	win32api.SetCursorPos((x_pad + coord[0], y_pad + coord[1]))
@@ -59,12 +65,58 @@ def get_play_area():
 
 	raise Exception("Play area not in view. " 
 			"Make sure the game is visible on screen!")
-		
+
+def count_nonblack_pil(img):
+	img = ImageOps.grayscale(img)
+	a = array(img.getcolors())
+	a = a.sum()
+	return a
+	
+def convert_box(box):
+	x,y,x_end,y_end = box
+	box = (x+x_pad, y+y_pad, x_end+x_pad, y_end+y_pad)
+	return box
+	
+def get_state(box):
+	im = ImageGrab.grab(convert_box(box))
+	return count_nonblack_pil(im)
 		
 def choose_player():
 	mouse_pos(random_coords(Coord.pvp[randint(0,5)]))
 	left_click()
-		
+
+def battle():
+	if get_state(Coord.battle_auto) != 33235:
+		mouse_pos(random_coords(Coord.battle_auto))
+		left_click()
+		time.sleep(uniform(.5,2.0))
+	mouse_pos(random_coords(Coord.battle_skip))
+	left_click()
+	
+def end_battle():
+	mouse_pos(random_coords(Coord.battle_end))
+	left_click()
+
+def end_daily_win():
+	mouse_pos(random_coords(Coord.state_daily_win))
+	left_click()
+	
+def end_stam():
+	mouse_pos(random_coords(Coord.state_stam_out))
+	left_click()
+	
+def resume():
+	mouse_pos(random_coords(Coord.menu_mission))
+	left_click()
+	time.sleep(4)
+	mouse_pos(random_coords(Coord.menu_battle))
+	
+def go_to_menu():
+	mouse_pos(random_coords(Coord.state_invalid))
+	left_click()
+	time.sleep(2)
+	resume()
+
 def find_state():
 	'''
 	Iterates through possible states and finds
@@ -75,21 +127,60 @@ def find_state():
 	DailyWin, Mission.
 	
 	'''
-	pass
+	global win, lose, quit
+	print "pvp:    " + str(get_state(Coord.state_pvp)) + ", " + "7221"
+	print "battle: " + str(get_state(Coord.state_battle)) + ", " + "4700"
+	print "win:    " + str(get_state(Coord.state_win)) + ", " + "1597"
+	print "lose:   " + str(get_state(Coord.state_lose)) + ", " + "18695"
+	print
+	if get_state(Coord.state_pvp) == 7221:
+		choose_player()
+	elif get_state(Coord.state_battle) == 4700:
+		battle()
+	elif get_state(Coord.state_win) == 513:
+		win = win + 1
+		print "Wins : " + str(win)
+		print "Loses: " + str(lose)
+		print "%    : " + str(((win * 1.0 /(win+lose)) * 100.0))
+		end_battle()
+	elif get_state(Coord.state_lose) == 18695:
+		lose = lose + 1
+		print "Wins : " + str(win)
+		print "Loses: " + str(lose)
+		print "%    : " + str(((win * 1.0 /(win+lose)) * 100.0))
+		end_battle()
+	elif get_state(Coord.state_daily_win) == 31261:
+		end_daily_win()
+	elif get_state(Coord.state_stam_out) == 23211:
+		end_stam()
+		time.sleep(randint(3600, 7200))
+		resume()
+	elif get_state(Coord.state_invalid) == 25685:
+		go_to_menu()
+	else:
+		print "no state found"
+		quit = True
+		
 		
 		
 def run():
 	find_state()
-	pass
+	time.sleep(uniform(.5,2.0))
 		
 def main():
+	global quit
 	#get_play_area()
 	#get_coords()
 	#mouse_pos(random_coords(Coord.menu_mission))
-	#box = get_play_area()
+	box = get_play_area()
+	#box = convert_box(Coord.state_invalid)
 	#im = ImageGrab.grab(box)
-	while True:
+	#im.save(os.getcwd() + '\\Snap__' + str(int(time.time())) + '.png', 'PNG')
+	#print count_nonblack_pil(im)
+	while not quit:
 		run()
+	im = ImageGrab.grab(box)
+	im.save(os.getcwd() + '\\Snap__' + str(int(time.time())) + '.png', 'PNG')
 	
 if __name__ == '__main__':
 	main()
--- coord.py
+++ coord.py
@@ -1,6 +1,7 @@
 class Coord:
 	
 	# --------------menu---------------#
+	
 	menu_battle  = (508, 803, 589, 881)
 	menu_mission = (404, 803, 487, 881)
 	menu_upgrade = (617, 803, 696, 881)
@@ -17,6 +18,21 @@ class Coord:
 	pvp_five  = (440, 508, 764, 711)
 	pvp_six   = (799, 508, 1126, 711)
 	pvp = [pvp_one, pvp_two, pvp_three, pvp_four, pvp_five, pvp_six]
+	
 	# -------------battle--------------#
+	
+	battle_auto = (1096, 797, 1179, 877) # 33235
+	battle_skip = (1107, 407, 1198, 501)
+	battle_end  = (563, 658, 639, 730)
+	
+	# -------------states--------------#
+	
+	state_pvp       = (54, 220, 93, 260)   # 7221
+	state_battle    = (8, 495, 48, 530)    # 4700
+	state_win       = (404, 455, 411, 459) # 1597
+	state_lose      = (396, 344, 420, 356) # 19331 or 18695
+	state_daily_win = (563, 515, 639, 595) # 31261
+	state_stam_out  = (840, 313, 913, 356) # 23211
+	state_invalid   = (505, 550, 700, 580) # 
 	
 	# -------------store---------------#
